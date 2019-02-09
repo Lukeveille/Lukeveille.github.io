ff5f6f8e73884bc248d1153df8b643079890f15f
Lukeveille <luke@lukeleveille.com>
2019-02-09 18:30:39 -0500

    Updates

---
+++ precache-manifest.85fe4e510c7df9a2251044de3cc4ea3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b8f72a56fb889717dba3bf693264836",
+    "url": "/static/media/monitor-frame-498x291.8b8f72a5.png"
+  },
+  {
+    "revision": "0e404be537d800b6ef5f",
+    "url": "/static/css/main.0711e7d5.chunk.css"
+  },
+  {
+    "revision": "0aeff715dc4ff4eb5a75",
+    "url": "/static/js/1.0aeff715.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e9da566c415d5098af248a360412ce6",
+    "url": "/static/media/profile-pic.2e9da566.jpg"
+  },
+  {
+    "revision": "8c1e5267e7afe9980d2f65ad4182f237",
+    "url": "/static/media/mobile-frame-291x144.8c1e5267.png"
+  },
+  {
+    "revision": "0e404be537d800b6ef5f",
+    "url": "/static/js/main.0e404be5.chunk.js"
+  },
+  {
+    "revision": "e060bf0a3af4f1a405137c5ec1fe3b3a",
+    "url": "/static/media/mrplow-desktop.e060bf0a.png"
+  },
+  {
+    "revision": "2888c39a8442f59a7474025a2837d465",
+    "url": "/static/media/mrplow-mobile.2888c39a.png"
+  },
+  {
+    "revision": "3c963bb96dee723857d2ec9be7a7d69e",
+    "url": "/static/media/mathquiz-desktop.3c963bb9.png"
+  },
+  {
+    "revision": "c8c8499ab7844816de7603c00b44e56d",
+    "url": "/static/media/mathquiz-mobile.c8c8499a.png"
+  },
+  {
+    "revision": "2f99917f4747f7021ecc0b4079424004",
+    "url": "/index.html"
+  }
+];
