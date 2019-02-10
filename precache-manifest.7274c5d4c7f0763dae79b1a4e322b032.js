50b45b9212f24aa3a09450c7ee999ddf0e38b01b
Lukeveille <luke@lukeleveille.com>
2019-02-09 21:41:14 -0500

    added production build from create-react-app

---
+++ precache-manifest.7274c5d4c7f0763dae79b1a4e322b032.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b8f72a56fb889717dba3bf693264836",
+    "url": "./static/media/monitor-frame-498x291.8b8f72a5.png"
+  },
+  {
+    "revision": "aad60c25a9d92a5eb3cf",
+    "url": "./static/css/main.3ad86ee1.chunk.css"
+  },
+  {
+    "revision": "0aeff715dc4ff4eb5a75",
+    "url": "./static/js/1.0aeff715.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2e9da566c415d5098af248a360412ce6",
+    "url": "./static/media/profile-pic.2e9da566.jpg"
+  },
+  {
+    "revision": "8c1e5267e7afe9980d2f65ad4182f237",
+    "url": "./static/media/mobile-frame-291x144.8c1e5267.png"
+  },
+  {
+    "revision": "aad60c25a9d92a5eb3cf",
+    "url": "./static/js/main.aad60c25.chunk.js"
+  },
+  {
+    "revision": "e060bf0a3af4f1a405137c5ec1fe3b3a",
+    "url": "./static/media/mrplow-desktop.e060bf0a.png"
+  },
+  {
+    "revision": "2888c39a8442f59a7474025a2837d465",
+    "url": "./static/media/mrplow-mobile.2888c39a.png"
+  },
+  {
+    "revision": "3c963bb96dee723857d2ec9be7a7d69e",
+    "url": "./static/media/mathquiz-desktop.3c963bb9.png"
+  },
+  {
+    "revision": "c8c8499ab7844816de7603c00b44e56d",
+    "url": "./static/media/mathquiz-mobile.c8c8499a.png"
+  },
+  {
+    "revision": "026b096edd40f1dedd1c5b7bafc08c80",
+    "url": "./index.html"
+  }
+];
